22283f84be6edad3846bde7336a112a0c58a1ec7
Samuel Unterrheiner <samuel.unterrheiner888@gamail.com>
2025-08-09 16:38:14 +1000

    First Upload

---
+++ include/robot-config.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "lemlib/api.hpp"
+#include "main.h"
+#include "pros/rtos.hpp"
+
+extern pros::Controller controller;
+// motor groups
+extern pros::MotorGroup leftMotors;
+extern pros::MotorGroup rightMotors;
+// lemlib defs
+extern lemlib::TrackingWheel horizontal;
+extern lemlib::TrackingWheel vertical;
+extern lemlib::Drivetrain drivetrain;
+extern lemlib::ControllerSettings lateralController;
+extern lemlib::ControllerSettings angularController;
+extern lemlib::OdomSensors sensors;
+extern lemlib::ExpoDriveCurve throttleCurve;
+extern lemlib::ExpoDriveCurve steerCurve;
+extern lemlib::Chassis chassis;
