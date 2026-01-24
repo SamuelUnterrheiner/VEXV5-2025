a6efa05c1d2191514dbdc14322d0d6924ea63ad1
Samuel Unterrheiner <samuel.unterrheiner888@gamail.com>
2026-01-24 12:02:58 +1100

    New Robot

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,45 @@
 #include "pros/rtos.hpp"
 #include "lemlib/api.hpp"
 
+
+// Rotation sensors
+pros::Rotation horizontalEncoder(17);
+pros::Rotation verticalEncoder(18);
+
+// Tracking wheels
+lemlib::TrackingWheel horizontal(
+    &horizontalEncoder,
+    lemlib::Omniwheel::NEW_275, // CHANGE if 3.25"
+    3.5 // inches from robot center (right = +, left = -)
+);
+
+lemlib::TrackingWheel vertical(
+    &verticalEncoder,
+    lemlib::Omniwheel::NEW_275, // CHANGE if 3.25"
+    4.0 // inches from robot center (forward = +, backward = -)
+);
+
+pros::IMU imu(16);
+// Odom sensors
+lemlib::OdomSensors sensors(
+    &vertical,   // vertical tracking wheel
+    nullptr,     // second vertical (unused)
+    &horizontal, // horizontal tracking wheel
+    nullptr,     // front tracking wheel
+    &imu         // IMU
+);
+
 pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup leftMotors({12, 14, 3});
+pros::MotorGroup rightMotors({-1, -15, -13});
+pros::Motor lowerintake(10);
+pros::Motor higherintake(2);
+pros::adi::DigitalOut frontbar('A');
+pros::adi::DigitalOut backoutlet('H');
+bool frontbarState = false;
+bool backoutletState = false;
 
-pros::MotorGroup leftMotors({1, 12, 13});
-pros::MotorGroup rightMotors({-15, -14, -3});
 
-pros::Motor intake(2);
 
 lemlib::Drivetrain drivetrain(
     &leftMotors,                  // left motor group
@@ -32,13 +65,6 @@ lemlib::ControllerSettings angularController(
 lemlib::ExpoDriveCurve throttleCurve(3, 10, 1.019);
 lemlib::ExpoDriveCurve steerCurve(3, 10, 1.019);
 
-lemlib::OdomSensors sensors(
-    nullptr,   // vertical tracking wheel 1
-    nullptr,   // vertical tracking wheel 2
-    nullptr,   // horizontal tracking wheel
-    nullptr,   // front tracking wheel (optional)
-    nullptr    // inertial sensor
-);
 
 lemlib::Chassis chassis(
     drivetrain,
@@ -49,7 +75,6 @@ lemlib::Chassis chassis(
     &steerCurve
 );
 void opcontrol() {
-
     while (true) {
         int drive = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int turn = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
@@ -62,12 +87,28 @@ void opcontrol() {
         chassis.tank(leftRPM, rightRPM);
 
         if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-            intake.move_velocity(200);
+            higherintake.move_velocity(200);
         else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-            intake.move_velocity(-200);
+            higherintake.move_velocity(-200);
         else
-            intake.move_velocity(0);
+            higherintake.move_velocity(0);
 
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+            lowerintake.move_velocity(200);
+        else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+            lowerintake.move_velocity(-200);
+        else
+            lowerintake.move_velocity(0);
+
+        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+            frontbarState = !frontbarState;
+            frontbar.set_value(frontbarState);
+        }
+
+        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+            backoutletState = !backoutletState;
+            backoutlet.set_value(backoutletState);
+        }
         pros::delay(25);
     }
 }
