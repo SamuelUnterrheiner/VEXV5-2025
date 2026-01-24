2d086440213c408927e014ef2f192a92d24c4a1d
Samuel Unterrheiner <samuel.unterrheiner888@gamail.com>
2026-01-24 13:08:06 +1100

    Toggle more fixes added

--- src/main.cpp
+++ src/main.cpp
@@ -5,53 +5,52 @@
 #include "pros/rtos.hpp"
 #include "lemlib/api.hpp"
 
-
-// Rotation sensors
 pros::Rotation horizontalEncoder(17);
 pros::Rotation verticalEncoder(18);
 
-// Tracking wheels
 lemlib::TrackingWheel horizontal(
     &horizontalEncoder,
-    lemlib::Omniwheel::NEW_275, // CHANGE if 3.25"
-    3.5 // inches from robot center (right = +, left = -)
+    lemlib::Omniwheel::NEW_275,
+    3.5
 );
 
 lemlib::TrackingWheel vertical(
     &verticalEncoder,
-    lemlib::Omniwheel::NEW_275, // CHANGE if 3.25"
-    4.0 // inches from robot center (forward = +, backward = -)
+    lemlib::Omniwheel::NEW_275,
+    4.0
 );
 
 pros::IMU imu(16);
-// Odom sensors
+
 lemlib::OdomSensors sensors(
-    &vertical,   // vertical tracking wheel
-    nullptr,     // second vertical (unused)
-    &horizontal, // horizontal tracking wheel
-    nullptr,     // front tracking wheel
-    &imu         // IMU
+    &vertical,
+    nullptr,
+    &horizontal,
+    nullptr,
+    &imu
 );
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
+
 pros::MotorGroup leftMotors({12, 14, 3});
 pros::MotorGroup rightMotors({-1, -15, -13});
+
 pros::Motor lowerintake(10);
 pros::Motor higherintake(2);
+
 pros::adi::DigitalOut frontbar('A');
 pros::adi::DigitalOut backoutlet('H');
+
 bool frontbarState = false;
 bool backoutletState = false;
 
-
-
 lemlib::Drivetrain drivetrain(
-    &leftMotors,                  // left motor group
-    &rightMotors,                 // right motor group
-    10.5,                         // track width in inches
-    lemlib::Omniwheel::NEW_325,   // wheel type
-    1000,                         // drivetrain RPM
-    2                             // horizontal drift
+    &leftMotors,
+    &rightMotors,
+    10.5,
+    lemlib::Omniwheel::NEW_325,
+    1000,
+    2
 );
 
 lemlib::ControllerSettings lateralController(
@@ -65,7 +64,6 @@ lemlib::ControllerSettings angularController(
 lemlib::ExpoDriveCurve throttleCurve(3, 10, 1.019);
 lemlib::ExpoDriveCurve steerCurve(3, 10, 1.019);
 
-
 lemlib::Chassis chassis(
     drivetrain,
     lateralController,
@@ -74,6 +72,19 @@ lemlib::Chassis chassis(
     &throttleCurve,
     &steerCurve
 );
+
+bool lowerintakeforwards = false;
+bool lowerintakereverse = false;
+bool intakeforwards = false;
+bool intakereverse = false;
+
+bool lastL1 = false;
+bool lastL2 = false;
+bool lastR1 = false;
+bool lastR2 = false;
+bool lastUp = false;
+bool lastDown = false;
+
 void opcontrol() {
     while (true) {
         int drive = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
@@ -84,31 +95,69 @@ void opcontrol() {
 
         int leftRPM = left * 200 / 127;
         int rightRPM = right * 200 / 127;
+
         chassis.tank(leftRPM, rightRPM);
 
-        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-            higherintake.move_velocity(200);
-        else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-            higherintake.move_velocity(-200);
-        else
-            higherintake.move_velocity(0);
+        bool L1 = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+        bool L2 = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+        bool R1 = master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+        bool R2 = master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+        bool UP = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
+        bool DOWN = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
 
-        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        if (L1 && !lastL1) {
+            lowerintakeforwards = !lowerintakeforwards;
+            lowerintakereverse = false;
+        }
+
+        if (L2 && !lastL2) {
+            lowerintakereverse = !lowerintakereverse;
+            lowerintakeforwards = false;
+        }
+
+        if (R1 && !lastR1) {
+            intakeforwards = !intakeforwards;
+            intakereverse = false;
+        }
+
+        if (R2 && !lastR2) {
+            intakereverse = !intakereverse;
+            intakeforwards = false;
+        }
+
+        if (lowerintakeforwards) {
             lowerintake.move_velocity(200);
-        else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+        } else if (lowerintakereverse) {
             lowerintake.move_velocity(-200);
-        else
+        } else {
             lowerintake.move_velocity(0);
+        }
 
-        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+        if (intakeforwards) {
+            higherintake.move_velocity(200);
+        } else if (intakereverse) {
+            higherintake.move_velocity(-200);
+        } else {
+            higherintake.move_velocity(0);
+        }
+
+        if (UP && !lastUp) {
             frontbarState = !frontbarState;
             frontbar.set_value(frontbarState);
         }
 
-        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+        if (DOWN && !lastDown) {
             backoutletState = !backoutletState;
             backoutlet.set_value(backoutletState);
         }
+
+        lastL1 = L1;
+        lastL2 = L2;
+        lastR1 = R1;
+        lastR2 = R2;
+        lastUp = UP;
+        lastDown = DOWN;
+
         pros::delay(25);
     }
 }
